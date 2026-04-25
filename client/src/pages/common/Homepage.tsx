429d078ff981c56e3f2a98e5639b5ca8a29eb1af
Davis-don <davismugoikou@gmail.com>
2026-04-25 17:40:54 +0300

    added

--- client/src/pages/common/Homepage.tsx
+++ client/src/pages/common/Homepage.tsx
@@ -1,9 +1,11 @@
 import './homepage.css'
 import Hero from '../../components/common/homepage/Hero'
+import Howto from '../../components/common/homepage/Howto'
 function Homepage() {
   return (
     <div className="overall-homepage-container">
       <Hero />
+      <Howto />
     </div>
   )
 }
