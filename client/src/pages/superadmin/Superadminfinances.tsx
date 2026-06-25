430f71b4d6f648e8a8580b90bf10d0a2f305987e
Davis-don <davismugoikou@gmail.com>
2026-06-25 12:55:33 +0300

    edited financial reports

--- client/src/pages/superadmin/Superadminfinances.tsx
+++ client/src/pages/superadmin/Superadminfinances.tsx
@@ -1,8 +1,31 @@
 import './superadminfinances.css'
+import { useState } from 'react'
+import Financialreportfilter from './Financialreportfilter'
+import Financialreportfetch from './Financialreportfetch'
 
 function Superadminfinances() {
+  const [filterYear, setFilterYear] = useState('')
+  const [filterMonth, setFilterMonth] = useState('')
+
+  const handleFilter = (year: string, month: string) => {
+    setFilterYear(year)
+    setFilterMonth(month)
+  }
+
+  const handleReset = () => {
+    setFilterYear('')
+    setFilterMonth('')
+  }
+
   return (
-    <div>Superadminfinances</div>
+    <div className="overall-super-admin-finances-container">
+      <div className="financial-report-filter-main">
+        <Financialreportfilter onFilter={handleFilter} onReset={handleReset} />
+      </div>
+      <div className="financial-report-fetch">
+        <Financialreportfetch year={filterYear} month={filterMonth} />
+      </div>
+    </div>
   )
 }
 
