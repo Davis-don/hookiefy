54e2cf4c368f45c912d0e522d5bb867b7da11df9
Davis-don <davismugoikou@gmail.com>
2026-08-10 21:36:10 +0300

    updated

--- client/src/data/dafaultdata.ts
+++ client/src/data/dafaultdata.ts
@@ -1,116 +1,186 @@
 // ============================================================
-// defaultdata.ts - African women with seductive bios for DefaultPostsdiv
+// defaultdata.ts - Kenyan women with seductive bios for DefaultPostsdiv
+// ============================================================
+
+// ============================================================
+// KENYAN FIRST NAMES (All female names from various communities)
 // ============================================================
 
 export const firstNames = [
-  'Amina', 'Fatima', 'Zara', 'Nia', 'Khadija', 
-  'Imani', 'Aisha', 'Yara', 'Layla', 'Nala', 
-  'Zuri', 'Amara', 'Kira', 'Sanaa', 'Zola', 
-  'Amani', 'Jada', 'Talia', 'Zahra', 'Nia'
+  // Luo names
+  'Akinyi', 'Atieno', 'Auma', 'Awino', 'Adhiambo',
+  'Anyango', 'Achieng', 'Akoth', 'Amondi', 'Apiyo',
+  
+  // Kikuyu names
+  'Njeri', 'Wanjiru', 'Muthoni', 'Nyambura', 'Wambui',
+  'Wairimu', 'Wangari', 'Njuguna', 'Mwangi', 'Kamau',
+  
+  // Luhya names
+  'Nekesa', 'Nasimiyu', 'Weche', 'Khadija', 'Fatuma',
+  'Kiptoo', 'Chepchumba', 'Chelangat', 'Kosgei', 'Jepchumba',
+  
+  // Kamba names
+  'Kaluki', 'Mawia', 'Ndanu', 'Mutheu', 'Syombua',
+  'Kanini', 'Kanyua', 'Zawadi', 'Baraka', 'Amani'
 ]
+
+// ============================================================
+// KENYAN LAST NAMES (Common surnames)
+// ============================================================
 
 export const lastNames = [
-  'Diallo', 'Kone', 'Diop', 'Traore', 'Kamara', 
-  'Sow', 'Bamba', 'Sissoko', 'Toure', 'Cisse', 
-  'Kante', 'Kouyate', 'Sangare', 'Sidibe', 'Diarra', 
-  'Keita', 'Drame', 'Konate', 'Doumbia', 'Sacko'
+  // Luo surnames
+  'Ochieng', 'Odhiambo', 'Omondi', 'Otieno', 'Onyango',
+  'Okoth', 'Ouma', 'Oduor', 'Odero', 'Opondo',
+  
+  // Kikuyu surnames
+  'Mwangi', 'Njoroge', 'Kariuki', 'Kamau', 'Maina',
+  'Muthoni', 'Wanjiru', 'Njeri', 'Wambui', 'Wangari',
+  
+  // Luhya surnames
+  'Weche', 'Wanyonyi', 'Masinde', 'Mukhwana', 'Khaemba',
+  'Kiptoo', 'Kiprop', 'Kipchumba', 'Kosgei', 'Chepkwony',
+  
+  // Kamba surnames
+  'Ndeti', 'Muoki', 'Kithuka', 'Kyalo', 'Mwololo',
+  
+  // Swahili/Arabic surnames
+  'Ali', 'Hassan', 'Mohammed', 'Bakari', 'Juma'
 ]
+
+// ============================================================
+// KENYAN LOCATIONS (Counties and cities)
+// ============================================================
 
 export const locations = [
-  'Nairobi', 'Lagos', 'Johannesburg', 'Accra', 'Dakar', 
-  'Abidjan', 'Kampala', 'Dar es Salaam', 'Cape Town', 'Addis Ababa', 
-  'Bamako', 'Kigali', 'Brazzaville', 'Windhoek', 'Maputo', 
-  'Libreville', 'Lusaka', 'Harare', 'Gaborone', 'Mombasa'
+  'Nairobi', 'Mombasa', 'Kisumu', 'Nakuru', 'Eldoret',
+  'Thika', 'Malindi', 'Kitale', 'Garissa', 'Nyeri',
+  'Meru', 'Embu', 'Kakamega', 'Bungoma', 'Busia',
+  'Homa Bay', 'Migori', 'Kisii', 'Nyamira', 'Siaya',
+  'Vihiga', 'Kitui', 'Machakos', 'Makueni', 'Kajiado',
+  'Narok', 'Bomet', 'Kericho', 'Uasin Gishu', 'Trans Nzoia',
+  'Lamu', 'Kwale', 'Kilifi', 'Tana River', 'Isiolo',
+  'Samburu', 'Turkana', 'Marsabit', 'Wajir', 'Mandera',
+  'Diani', 'Watamu', 'Naivasha', 'Nanyuki', 'Nyahururu'
 ]
 
-// Seductive bios - fun, flirty, and inviting
+// ============================================================
+// SEDUCTIVE BIOS - Fun, flirty, inviting with Kenyan flavor
+// ============================================================
+
 export const bios = [
-  'Looking for someone fun tonight 😏 Let\'s make it memorable 💋',
-  'Bored and lonely... come keep me company tonight 🔥',
-  'Feeling naughty and looking for trouble 😈 Who\'s down?',
-  'Just a sexy African queen looking for her king tonight 👑',
-  'Your place or mine? 😘 Let\'s have some fun',
+  'Nairobi girl looking for someone fun tonight 😏 Let\'s make it memorable 💋',
+  'Bored and lonely in Kisumu... come keep me company tonight 🔥',
+  'Feeling naughty in Mombasa 😈 Who\'s down to vibe?',
+  'Just a sexy Kenyan queen looking for her king tonight 👑',
+  'Your place or mine? 😘 Let\'s have some fun in Nairobi',
   'Ready to misbehave tonight 😏 Show me what you got',
-  'Looking for a real man to handle all of this 🔥',
-  'Sexy and single... looking for a good time tonight 💋',
-  'Come take me out and see where the night goes 🌙',
-  'Feeling freaky tonight... who\'s brave enough? 😈',
-  'Just a bad girl looking for a good time 🔥',
-  'Your dream girl is waiting... don\'t keep me waiting 💋',
-  'Ready to get wild and crazy tonight 😏 Who\'s with me?',
-  'Looking for someone to make me smile... and more 😘',
-  'Africa\'s finest 🔥 Come get a taste of paradise',
-  'Feeling lonely and needy... need a real man tonight 💋',
-  'Let\'s skip the small talk and get straight to the fun 😏',
-  'Just a naughty girl with a good heart ❤️‍🔥 Come play with me',
-  'Ready for an adventure tonight 🌙 Who\'s taking me out?',
-  'Looking for a good time and great company 🍷 Let\'s vibe'
+  'Looking for a real man to handle all of this 🔥 Ni mimi?',
+  'Sexy and single in Nakuru... looking for a good time tonight 💋',
+  'Come take me out and see where the night goes 🌙 Sherehe!',
+  'Feeling freaky tonight in Eldoret... who\'s brave enough? 😈',
+  'Just a bad girl from Kisii looking for a good time 🔥',
+  'Your dream girl is waiting in Thika... don\'t keep me waiting 💋',
+  'Ready to get wild and crazy tonight in Malindi 😏 Who\'s with me?',
+  'Looking for someone to make me smile... and more in Mombasa 😘',
+  'Kenya\'s finest 🔥 Come get a taste of paradise at the coast',
+  'Feeling lonely in Kitale... need a real man tonight 💋',
+  'Let\'s skip the small talk and get straight to the fun in Nairobi 😏',
+  'Just a naughty girl with a good heart ❤️‍🔥 Come play with me in Kisumu',
+  'Ready for an adventure tonight in Diani 🌙 Who\'s taking me out?',
+  'Looking for a good time and great company 🍷 Let\'s vibe in Westlands',
+  'Nakuru girl with a wild side 🔥 Come find me tonight',
+  'Mombasa beaches got me feeling some type of way 😏',
+  'Eldoret darling ready to paint the town red tonight 💋',
+  'Thika beauty looking for someone to spoil me 😘',
+  'Kisumu queen with a taste for adventure and good times 🌙'
 ]
+
+// ============================================================
+// ALL FEMALE GENDERS
+// ============================================================
 
 export const genders = [
-  'F', 'F', 'F', 'F', 'F', 
-  'F', 'F', 'F', 'F', 'F', 
-  'F', 'F', 'F', 'F', 'F', 
+  'F', 'F', 'F', 'F', 'F',
+  'F', 'F', 'F', 'F', 'F',
+  'F', 'F', 'F', 'F', 'F',
   'F', 'F', 'F', 'F', 'F'
 ]
 
+// ============================================================
+// TIME OPTIONS
+// ============================================================
+
 export const timeOptions = [
-  '2 hours ago', '3 hours ago', '5 hours ago', '7 hours ago', 
-  '12 hours ago', '1 day ago', '2 days ago', '3 days ago', 
-  '5 days ago', '1 week ago'
+  '2 hours ago', '3 hours ago', '5 hours ago', '7 hours ago',
+  '12 hours ago', '1 day ago', '2 days ago', '3 days ago',
+  '5 days ago', '1 week ago', '2 weeks ago'
 ]
 
-// African women images - using reliable placeholder images with African models
+// ============================================================
+// KENYAN WOMEN IMAGES - Using reliable African model images
+// These are hosted on reliable CDN services
+// ============================================================
+
 export const imageUrls = [
-  'https://images.pexels.com/photos/1454167/pexels-photo-1454167.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454309/pexels-photo-1454309.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462983/pexels-photo-1462983.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462984/pexels-photo-1462984.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704489/pexels-photo-1704489.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454168/pexels-photo-1454168.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462982/pexels-photo-1462982.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704490/pexels-photo-1704490.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454169/pexels-photo-1454169.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704487/pexels-photo-1704487.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462985/pexels-photo-1462985.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454170/pexels-photo-1454170.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704491/pexels-photo-1704491.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462981/pexels-photo-1462981.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454308/pexels-photo-1454308.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704492/pexels-photo-1704492.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1462986/pexels-photo-1462986.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1454171/pexels-photo-1454171.jpeg?w=800&h=1000&fit=crop',
-  'https://images.pexels.com/photos/1704493/pexels-photo-1704493.jpeg?w=800&h=1000&fit=crop'
+  // Using images from reliable sources
+  'https://cdn.pixabay.com/photo/2016/11/21/14/52/woman-1845886_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/01/08/18/25/woman-593373_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2014/11/13/17/04/woman-529613_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/06/02/22/32/woman-1431175_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/10/13/15/48/woman-986406_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072908_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869761_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/07/09/22/55/woman-838700_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/08/23/08/36/woman-1613630_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/14/03/10/beautiful-1822515_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2014/12/05/18/45/woman-558102_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/04/05/18/30/woman-1309798_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/01/26/07/01/woman-1160638_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/22/19/12/woman-1850134_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2015/07/19/08/45/woman-851383_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869760_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/17/46/woman-1836133_960_720.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/14/04/08/woman-1822541_960_720.jpg'
 ]
+
+// ============================================================
+// AVATAR IMAGES (Smaller versions of the same images)
+// ============================================================
 
 export const avatarUrls = [
-  'https://images.pexels.com/photos/1454167/pexels-photo-1454167.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454309/pexels-photo-1454309.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462983/pexels-photo-1462983.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704488/pexels-photo-1704488.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462984/pexels-photo-1462984.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704489/pexels-photo-1704489.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454168/pexels-photo-1454168.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462982/pexels-photo-1462982.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704490/pexels-photo-1704490.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454169/pexels-photo-1454169.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704487/pexels-photo-1704487.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462985/pexels-photo-1462985.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454170/pexels-photo-1454170.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704491/pexels-photo-1704491.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462981/pexels-photo-1462981.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454308/pexels-photo-1454308.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704492/pexels-photo-1704492.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1462986/pexels-photo-1462986.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1454171/pexels-photo-1454171.jpeg?w=100&h=100&fit=crop',
-  'https://images.pexels.com/photos/1704493/pexels-photo-1704493.jpeg?w=100&h=100&fit=crop'
+  'https://cdn.pixabay.com/photo/2016/11/21/14/52/woman-1845886_640.jpg',
+  'https://cdn.pixabay.com/photo/2015/01/08/18/25/woman-593373_640.jpg',
+  'https://cdn.pixabay.com/photo/2014/11/13/17/04/woman-529613_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/06/02/22/32/woman-1431175_640.jpg',
+  'https://cdn.pixabay.com/photo/2015/10/13/15/48/woman-986406_640.jpg',
+  'https://cdn.pixabay.com/photo/2017/02/16/23/10/smile-2072908_640.jpg',
+  'https://cdn.pixabay.com/photo/2017/07/31/11/21/people-2557396_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869761_640.jpg',
+  'https://cdn.pixabay.com/photo/2015/07/09/22/55/woman-838700_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/08/23/08/36/woman-1613630_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/14/03/10/beautiful-1822515_640.jpg',
+  'https://cdn.pixabay.com/photo/2014/12/05/18/45/woman-558102_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/04/05/18/30/woman-1309798_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/01/26/07/01/woman-1160638_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/22/19/12/woman-1850134_640.jpg',
+  'https://cdn.pixabay.com/photo/2015/07/19/08/45/woman-851383_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/29/13/14/attractive-1869760_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/18/17/46/woman-1836133_640.jpg',
+  'https://cdn.pixabay.com/photo/2016/11/14/04/08/woman-1822541_640.jpg'
 ]
+
+// ============================================================
+// INTEREST OPTIONS (All interested in Men)
+// ============================================================
 
 export const interestedOptions = ['Men', 'Men', 'Men', 'Men', 'Men', 'Men', 'Men', 'Men', 'Men', 'Men']
 
 // ============================================================
-// GENERATE RANDOM USERS
+// TYPES
 // ============================================================
 
 export interface DefaultUser {
@@ -127,6 +197,10 @@ export interface DefaultUser {
   min_age: number
   max_age: number
 }
+
+// ============================================================
+// GENERATE RANDOM USERS
+// ============================================================
 
 export function generateRandomUsers(count: number = 20): DefaultUser[] {
   const users: DefaultUser[] = []
@@ -153,5 +227,10 @@ export function generateRandomUsers(count: number = 20): DefaultUser[] {
   return users
 }
 
-// Export default users for easy import
+// ============================================================
+// EXPORT DEFAULT USERS
+// ============================================================
+
 export const defaultUsers = generateRandomUsers(20)
+
+export default defaultUsers
