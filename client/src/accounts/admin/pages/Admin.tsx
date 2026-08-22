18e8956b0d561f581382ff8dcd2279de83dd1ab8
Davis-don <davismugoikou@gmail.com>
2026-08-22 17:13:10 +0300

    updated

---
+++ client/src/accounts/admin/pages/Admin.tsx
@@ -0,0 +1,10 @@
+import './admin.css'
+function Admin() {
+  return (
+    <div className="overall-admin-container-account">
+        <h1>Admin accout</h1>
+    </div>
+  )
+}
+
+export default Admin
---
+++ client/src/accounts/superadmin/pages/Superadmin.tsx
@@ -0,0 +1,11 @@
+import './superadmin.css'
+
+function Superadmin() {
+  return (
+    <div className="overall-super-admin-account-container">
+        <h1>Super admin account</h1>
+    </div>
+  )
+}
+
+export default Superadmin
