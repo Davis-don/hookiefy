0046e9cdb4d3f7cfd50ef36ea44a75d418d0d323
Davis-don <davismugoikou@gmail.com>
2026-08-22 21:46:11 +0300

    updated

---
+++ client/src/Accounts/clients/components/Editclientprofile.tsx
@@ -0,0 +1,12 @@
+import './editclientprofile.css'
+import Loadingcomponent from '../../common/components/Loading/Loadingcomponent'
+
+function Editclientprofile() {
+  return (
+   <div className="overall-edit-user-profile-container">
+    <Loadingcomponent/>
+   </div>
+  )
+}
+
+export default Editclientprofile
--- client/src/Accounts/clients/components/Profiledatarender.tsx
+++ client/src/Accounts/clients/components/Profiledatarender.tsx
@@ -1,11 +1,19 @@
 import './profiledatarender.css'
 import Addimageprofile from './Addimageprofile'
 import Profiledetailfetchview from './Profiledetailfetchview'
+import Editclientprofile from './Editclientprofile'
+import 'bootstrap/dist/css/bootstrap-grid.min.css'
+import { useState } from 'react'
 function Profiledatarender() {
+  const [mountEdit, setMountedit]=useState(false)
   return (
     <div className="profile-datarender-container-overall">
         <Addimageprofile/>
-        <Profiledetailfetchview/>
+        {mountEdit ? <Editclientprofile/> : <Profiledetailfetchview/>}
+        <div className="button-edit-profile-container">
+      <button  onClick={()=>setMountedit(!mountEdit)} className={mountEdit?'btn btn-primary':"btn btn-secondary"}>{mountEdit ? "View Profile" : "Edit profile"}</button>
+        </div>
+        
     </div>
   )
 }
