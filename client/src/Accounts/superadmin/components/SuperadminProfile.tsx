94af27a371c4c2fc31f601d06d8502cf1cf5b4bd
Davis-don <davismugoikou@gmail.com>
2026-09-17 16:39:41 +0300

    edited profile

--- client/src/Accounts/superadmin/components/SuperadminProfile.tsx
+++ client/src/Accounts/superadmin/components/SuperadminProfile.tsx
@@ -1,10 +1,14 @@
 import Addimageprofile from '../../common/components/Addimageprofile'
+import Generalinfoedit from '../../common/components/Generalinfoedit'
+import Passwordedit from '../../common/components/Passwordedit'
 import './superadminprofile.css'
 
 function SuperadminProfile() {
   return (
     <div className="overall-superadmin-profile">
       <Addimageprofile/>
+      <Generalinfoedit/>
+      <Passwordedit/>
     </div>
   )
 }
